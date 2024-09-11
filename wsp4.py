fd63fd0c11b2f4b89a77a08507c898216fb1723f
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-09-11 11:41:01 +0530

    Update wsp4.py

--- wsp4.py
+++ wsp4.py
@@ -12,28 +12,16 @@ from scipy import stats
 from statsmodels.tsa.arima.model import ARIMA
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import Paragraph
 from streamlit_lottie import st_lottie
 import json
 import requests
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, PageBreak
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, PageBreak
 def load_lottie_url(url: str):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
-
-
 def create_stats_pdf(stats_data, district):
     buffer = BytesIO()
     doc = SimpleDocTemplate(buffer, pagesize=letter)
