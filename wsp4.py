b1a4324d15a62d5bbe5fec2cd5d9759449b14c7d
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-09-11 11:03:52 +0530

    Update wsp4.py

--- wsp4.py
+++ wsp4.py
@@ -22,6 +22,10 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
 
 def load_lottie_url(url: str):
     r = requests.get(url)
