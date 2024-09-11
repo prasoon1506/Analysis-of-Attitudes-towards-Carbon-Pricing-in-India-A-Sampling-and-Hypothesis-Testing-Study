abccc9da483672ac3d940cb6a4fe87ad6f2d66c0
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-09-11 11:34:51 +0530

    Update wsp4.py

--- wsp4.py
+++ wsp4.py
@@ -363,7 +363,13 @@ def plot_district_graph(df, district_names, benchmark_brands_dict, desired_diff_
         with open("district_plots.pdf", "rb") as f:
             pdf_data = f.read()
         b64_pdf = base64.b64encode(pdf_data).decode()
-        st.download_button(f'<a download="{region_name}.pdf" href="data:application/pdf;base64,{b64_pdf}">Download All Plots as PDF</a>', unsafe_allow_html=True)
+        st.markdown(f'<a download="{region_name}.pdf" href="data:application/pdf;base64,{b64_pdf}">Download All Plots as PDF</a>', unsafe_allow_html=True)
+        st.download_button(
+                    label="Download All Stats and Predictions PDF",
+                    data=district_plots.getvalue(),
+                    file_name=f"{selected_districts}stats_and_predictions.pdf",
+                    mime="application/pdf"
+                )
 
 def update_week_name(index):
     def callback():
