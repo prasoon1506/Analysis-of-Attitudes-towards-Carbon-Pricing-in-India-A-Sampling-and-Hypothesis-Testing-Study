adc08b2e55779097f2d2a6b06c8671a8967e4a93
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-09-13 17:01:01 +0530

    Update wsp4.py

--- wsp4.py
+++ wsp4.py
@@ -1421,7 +1421,7 @@ def display_data(df, selected_regions, selected_districts, selected_channels, sh
         # Add a line chart for monthly trends including September 2024
         months = ['Apr', 'May', 'Jun', 'Jul', 'Aug','Sep']
         fig_trend = go.Figure()
-        for values in ['23','24']:
+        for year in ['23','24']:
             y_values = [grouped_data[f'{year}-{month}{suffix}'].sum() for month in months]
             if year == '24':
                 y_values[-1] = None
