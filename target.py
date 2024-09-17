cc4d862a8e1d91cd4b49f607879db29fde495dba
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-09-17 17:05:52 +0530

    Update target.py

--- target.py
+++ target.py
@@ -312,7 +312,7 @@ def create_visualization(region_data, region, brand, months, sept_target, sept_a
     
     ax4.pie(region_type_data, labels=region_type_labels, colors=colors,
             autopct=make_autopct(region_type_data), startangle=90)
-    ax4.set_title('August Region Type Breakdown:-', fontsize=14, fontweight='bold')
+    ax4.set_title('August Region Type Breakdown:-', fontsize=15, fontweight='bold')
     
     plt.tight_layout()
     return fig
