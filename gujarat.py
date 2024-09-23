7a9aac7deafd2ebc46cb9e1a6da9253fdf0a89c5
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-09-23 10:02:32 +0530

    Update gujarat.py

--- gujarat.py
+++ gujarat.py
@@ -46,7 +46,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Title and description
-st.title("📊 Interactive Data Analysis App")
+st.title("📊 Normal Vs. Premium Product Analysis")
 st.markdown("Upload your Excel file and analyze it with interactive visualizations.")
 st.markdown("<div class='upload-section'>", unsafe_allow_html=True)
 col1, col2 = st.columns([2, 1])
