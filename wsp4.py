09847af78c61575f59fe10e3c3e5469a161b4fcc
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-10-18 15:16:36 +0530

    Update wsp4.py

--- wsp4.py
+++ wsp4.py
@@ -2173,15 +2173,15 @@ def create_visualization(region_data, region, brand, months):
         if pct_change > 0:
             arrow_style = 'fancy,head_length=4,head_width=6'
             arrow_color = 'green'
-            start_x = x + 0.07
-            end_x = x + width * 0.47
+            start_x = x + 0.15
+            end_x = x + width * 0.49
             start_y = y + 0.30
             end_y = y + 0.30
         else:
             arrow_style = 'fancy,head_length=4,head_width=6'
             arrow_color = 'red'
-            start_x = x + 0.07
-            end_x = x + width * 0.47
+            start_x = x + 0.15
+            end_x = x + width * 0.49
             start_y = y + 0.30
             end_y = y + 0.30
             
