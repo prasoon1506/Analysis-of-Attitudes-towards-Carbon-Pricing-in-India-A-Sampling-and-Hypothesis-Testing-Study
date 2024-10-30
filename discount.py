c0069843778de2e678b498272207de7ae8a8a82d
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-10-30 14:28:08 +0530

    Update discount.py

--- discount.py
+++ discount.py
@@ -36,7 +36,7 @@ st.markdown("""
     
     .ticker-content {
         display: inline-block;
-        animation: ticker 1000s linear infinite;
+        animation: ticker 100s linear infinite;
         animation-play-state: running;
         padding-right: 100%;
     }
