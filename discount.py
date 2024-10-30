c2174d7575d584d3e74df23f6f8525d0d7022b4e
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-10-30 14:32:16 +0530

    Update discount.py

--- discount.py
+++ discount.py
@@ -47,7 +47,7 @@ st.markdown("""
     
     .ticker-item {
         display: inline-block;
-        margin-right: 2px;
+        margin-right: 80px;
         font-size: 16px;
         padding: 5px 10px;
     }
