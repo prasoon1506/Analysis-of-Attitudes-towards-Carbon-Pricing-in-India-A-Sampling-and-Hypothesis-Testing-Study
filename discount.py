b7803028293f78ace2eaf7efe0441ef543244b18
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-10-30 14:57:42 +0530

    Update discount.py

--- discount.py
+++ discount.py
@@ -36,8 +36,8 @@ st.markdown("""
     
     .ticker-content {
         display: inline-block;
-        animation: ticker 1200s linear infinite;  /* Set to 60 seconds */
-        animation-delay: -1190s;  /* Start halfway through to avoid initial wait */
+        animation: ticker 1000s linear infinite;  /* Set to 60 seconds */
+        animation-delay: -995s;  /* Start halfway through to avoid initial wait */
         padding-right: 100%;
         will-change: transform;
         transform: translateZ(0);
