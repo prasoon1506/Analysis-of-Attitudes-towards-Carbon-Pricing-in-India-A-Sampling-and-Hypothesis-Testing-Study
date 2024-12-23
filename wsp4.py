3e286c34c11fb50432289cee061714e38beb6732
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-12-23 10:38:57 +0530

    Update wsp4.py

--- wsp4.py
+++ wsp4.py
@@ -396,7 +396,7 @@ def price():
         return
     wsp_columns = ['D1-3', 'D4-6', 'D7-9', 'D10-12', 'D13-15','D16-18','D19-21']
     metric_values = region_wsp[wsp_columns].values.flatten().tolist()
-    week_labels = ['1-3 Dec', '4-6 Dec', '7-9 Dec', '10-12 Dec', '13-15 Dec','16-18 Dec','19-20 Nov']
+    week_labels = ['1-3 Dec', '4-6 Dec', '7-9 Dec', '10-12 Dec', '13-15 Dec','16-18 Dec','19-20 Dec']
     header_text = f"WSP Progression in December 2024" + \
                   (f" - {brand_name}" if brand_name else "")
     story.append(Paragraph(header_text + ":-", month_style))
