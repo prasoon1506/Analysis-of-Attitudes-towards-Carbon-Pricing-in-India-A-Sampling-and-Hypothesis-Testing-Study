960e777dc374df4b1e4832c229bffb9d15b8a34f
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2025-02-11 11:58:53 +0530

    Update bag.py

--- bag.py
+++ bag.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
-
 def main():
-    st.title("Cement Plant Bag Usage Analysis")
+    st.title("Cement Plant Bag Usage Analysis",layout=wide)
     
     # File uploader
     uploaded_file = st.file_uploader("Upload your Excel file", type=['xlsx', 'xls'])
